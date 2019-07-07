c509c1912d92ee431b12cc2b8b14e37f993c8288
zhangdan01 <zhangdan01@fenbi.com>
2019-07-07 10:11:33 +0800

    hello

---
+++ src/main/java/hell.java
@@ -0,0 +1,12 @@
+/**
+ * @(#)hell.java, Jul 07, 2019.
+ * <p>
+ * Copyright 2019 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * @author zhangdan
+ */
+public class hell {
+}
